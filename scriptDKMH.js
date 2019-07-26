23df4c4123f7b33c168a1ff5da858d1f66181419
Tuan Nguyen <tuananhnguyenyt@gmail.com>
2019-07-26 15:14:28 +0700

    push first file

---
+++ scriptDKMH.js
@@ -0,0 +1,18 @@
+var checkBox = document.querySelector(".order"); // node list : query tat ca cac mon hoc
+var data = [61,62,63]; // data mon hoc (demo) (rowindex cua mon hoc)
+
+function attack(count) {
+    if (count<200) {
+        setTimeout(function(){ // sau 1 time thi thuc thi
+            for (let i=0;i<checkBox.length;i++){
+                if (data.indexOf(checkBox[i].dataset.rowindex)!=-1)  // kiem tra xem mon hoc co trong list data hay khong
+                    checkBox[i].checked = true; // neu co thi tick vao mon do. buoc nay la buoc chon mon hoc va chua xac nhan
+            }
+            document.querySelector(".confirm-registration").click();// confirm tat ca mon hoc
+            attack(count++);
+        },0);
+    }
+    
+}
+attack(0); // excicute function
+// use : paste into dev tools console
