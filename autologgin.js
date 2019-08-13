b97b1f509192ff4a6209c42d19bbb12a4dc55a7a
Tuan Nguyen <tuananhnguyenyt@gmail.com>
2019-08-13 11:07:47 +0700

    auto loggin js

---
+++ autologgin.js
@@ -0,0 +1,9 @@
+var user = document.querySelector("#txtLoginId");
+var pass = document.querySelector("#txtPassword");
+var submit = document.querySelector(".button");
+if (window.location.pathname=="/dkmh/login.asp") {
+	user.value = "17021105";
+	pass.value = "15071999yt";
+	submit.click();
+
+}
--- scriptDKMH.js
+++ scriptDKMH.js
@@ -1,18 +1,29 @@
-var checkBox = document.querySelectorAll(".order"); // node list : query tat ca cac mon hoc
-var data = [61,62,63]; // data mon hoc (demo) (rowindex cua mon hoc)
+var checkBox = document.querySelectorAll("input.order"); // node list : query tat ca cac mon hoc
+var data = [68,69,57,215,216,210,653,541,599]; // data mon hoc (demo) (rowindex cua mon hoc)
 
 function attack(count) {
-    if (count<200) {
-        setTimeout(function(){ // sau 1 time thi thuc thi
+    
+        setInterval(function(){ // sau 1 time thi thuc thi
             for (let i=0;i<checkBox.length;i++){
                 if (data.indexOf(checkBox[i].dataset.rowindex)!=-1)  // kiem tra xem mon hoc co trong list data hay khong
                     checkBox[i].checked = true; // neu co thi tick vao mon do. buoc nay la buoc chon mon hoc va chua xac nhan
             }
-            document.querySelector(".confirm-registration").click();// confirm tat ca mon hoc, tuong tu voi $(".confirm-resgistration")["click"]();
+            document.querySelector(".confirm-registration").click();
+            console.log("success");// confirm tat ca mon hoc, tuong tu voi $(".confirm-resgistration")["click"]();
             attack(++count);
-        },0);
-    }
+        },10);
+
     
 }
+
 attack(0); // excicute function
 // use : paste into dev tools console
+var user = document.querySelector("#txtLoginId");
+var pass = document.querySelector("#txtPassword");
+var submit = document.querySelector("#frmLogin");
+if (window.location.pathname=="/dkmh/login.asp") {
+	user.value = "17021105";
+	pass.value = "15071999yt";
+	submit.submit();
+
+}
